5e7444840eb31cc521fe8b65c1bd1dec957c07e0
David Long <davdaarn@users.noreply.github.com>
2021-03-28 13:58:49 -0600

    changed styles

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
   <div id="app">
-    <navbar></navbar>
-    <router-view class="p-36"/>
+    <navbar class="top-0"></navbar>
+    <router-view class="p-36" />
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 export default {
- components: {
-   Navbar
- }
-}
+  components: {
+    Navbar
+  }
+};
 </script>
 
 
 <style lang="scss">
-
 </style>
